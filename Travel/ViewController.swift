8eb2640c273d5437d2bd0a2bc37534a85012a655
Jaka <jaka0926@naver.com>
2024-05-28 16:11:17 +0900

    Initial Commit

---
+++ Travel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by Jaka on 2024-05-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
